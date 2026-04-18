f99df488bf08bf3bdfd886dac1458fb3c8a9064e
test19124 <test19124@sina.com>
2026-04-18 23:38:29 +0800

    小修小补 从Hexo迁移

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     subtitle: "Hello world!",
     logo: "",
   },
+  cover: {
+    enableNextGradientCover: true,
+  },
   copyright: {
     license: "CC-BY-NC-SA-4.0", // 许可证类型
     show: true, // 是否显示版权声明
@@ -26,12 +29,14 @@ export default defineConfig({
     randomPosts: true, // 显示 10 篇随机文章
   },
   home: {
-    pageSize: 5, // 每页显示 5 篇文章
+    pageSize: 10, // 每页显示 x 篇文章
     selectedCategories: [
       // 突出显示分类
-      { 
+      {
         name: "编程",
-        cover: "/images/tutorial-cover.jpg"
+      },
+      {
+        name: "学习",
       },
     ],
   },
@@ -42,32 +47,42 @@ export default defineConfig({
       icon: "i-ri-home-line",
     },
     {
+      href: "/categories/",
+      text: "分类",
+      icon: "i-ri-book-shelf-fill",
+    },
+    {
+      href: "/tags/",
+      text: "标签",
+      icon: "i-ri-price-tag-3-fill",
+    },
+    {
+      href: "/archives/",
+      text: "归档",
+      icon: "i-ri-archive-line",
+    },
+    {
+      href: "/friends/",
+      text: "友情链接",
+      icon: "i-ri-link",
+    },
+    {
       text: "关于我",
       href: "/about/",
       icon: "i-ri-user-smile-line",
-    },
-    {
-      text: "文章",
-      href: "/posts/",
-      icon: "i-ri-quill-pen-fill",
       dropbox: {
         enable: true, // 启用下拉菜单
         items: [ // 子菜单内容
           {
-            href: "/categories/",
-            text: "分类",
-            icon: "i-ri-book-shelf-fill",
+            href: "https://github.com/test19124",
+            text: "Github",
+            icon: "i-ri-github-fill",
           },
           {
-            href: "/tags/",
-            text: "标签",
-            icon: "i-ri-price-tag-3-fill",
-          },
-          {
-            href: "/archives/",
-            text: "归档",
-            icon: "i-ri-archive-line",
-          },
+            href: "mailto:test19124@sina.com",
+            text: "Email",
+            icon: "i-ri-mail-line",
+          }
         ],
       },
     },
@@ -86,5 +101,25 @@ export default defineConfig({
         icon: "i-ri-mail-line",
       },
     },
+  },
+  friends: {
+    title: "友链",
+    description: "友链列表",
+    links: [
+      {
+        url: "https://www.kaitaku.xyz",
+        title: "望月阁",
+        desc: "一个萌新的学习笔记",
+        author: "Kaitaku",
+        avatar: "https://www.kaitaku.xyz/assets/avatar.avif",
+      },
+      {
+        url: "https://www.sprenedayf.com/",
+        title: "永恒绿洲",
+        desc: "虽然不容易，只要去做，就能成功",
+        author: "蝶风",
+        avatar: "https://i.imgtg.com/2023/04/04/2VcIc.jpg",
+      },
+    ],
   },
 });
